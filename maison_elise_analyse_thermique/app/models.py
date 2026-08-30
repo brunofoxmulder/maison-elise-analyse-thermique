d4fdf01af7e1d76637391a4cf2d9d3a05c04d769
brunofoxmulder <bruno@mbsimon.fr>
2026-08-30 02:03:01 +0200

    Distribuer models.py dev.3

---
+++ maison_elise_analyse_thermique/app/models.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+@dataclass(frozen=True)
+class ThermalSample:
+    ts: datetime
+    temp_indoor: Optional[float]
+    humidity_indoor: Optional[float]
+    temp_outdoor_ref: Optional[float]
+    humidity_outdoor: Optional[float]
+    setpoint: Optional[float]
+    hvac_mode: Optional[str]
+    hvac_action: Optional[str]
+    compressor_frequency: Optional[float]
+    compressor_energy_day: Optional[float]
+    cool_energy_last_hour: Optional[float]
+    heat_energy_last_hour: Optional[float]
+    lux: Optional[float]
+    sun_elevation: Optional[float]
+    sun_azimuth: Optional[float]
+    shutter_salon: Optional[float]
+    shutter_terrasse: Optional[float]
+    window_open: Optional[bool]
+    door_window_open: Optional[bool]
